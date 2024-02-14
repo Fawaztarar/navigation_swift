29faaa48167e4e6734fa899856f5c9dba31e1de2
Fawaztarar <fawaztarar@gmail.com>
2024-02-14 11:59:43 +0000

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Fawaz Tarar on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
